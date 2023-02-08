700fe66d99414e2d496e3fc1c9c8f6dea5286789
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-08 23:15:41 +0600

    Описал логику отправки в slice

--- frontend/src/store/shortLinkSlice.ts
+++ frontend/src/store/shortLinkSlice.ts
@@ -1,13 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 import {RootState} from "../app/store";
+import {postLink} from "./shortLinkThunk";
+import {Link} from "../types";
 
 interface ShortLinkState {
-  url: string;
+  url: Link | null;
   loading: boolean;
 }
 
 const initialState:ShortLinkState = {
-  url: '',
+  url: null,
   loading: false,
 }
 
@@ -16,7 +18,16 @@ export const shortLinkSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-
+    builder.addCase(postLink.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(postLink.fulfilled, (state, {payload: url}) => {
+      state.loading = false;
+      state.url = url;
+    });
+    builder.addCase(postLink.rejected, (state) => {
+      state.loading = false;
+    });
   }
 });
 
