f684ae7c82f8e5f368c64d8cf2dc414591fe66e9
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-08 18:16:45 +0600

    Подключил роутер и сделал connect к базе данным

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,26 @@
 import express from 'express';
+import mongoose from "mongoose";
+import shortRecordRouters from "./routes/shortRecord";
 
 const app = express();
 const port = 8000;
 
-app.listen(port, () => {
-  console.log('Server: ', port)
-});
+app.use(express.json());
+
+app.use('/', shortRecordRouters);
+app.use('/links', shortRecordRouters);
+
+const run = async () => {
+  mongoose.set('strictQuery', false);
+  await mongoose.connect('mongodb://localhost/short_record');
+
+  app.listen(port, () => {
+    console.log('Server: ', port)
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+}
+
+run().catch(console.error);
