3e1707acf1ab7797b6c09b1637a63db9a2773e8b
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-08 23:11:39 +0600

    Подключил cors для frontend

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from "mongoose";
+import cors from 'cors';
 import shortRecordRouters from "./routes/shortRecord";
 
 const app = express();
 const port = 8000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/', shortRecordRouters);
