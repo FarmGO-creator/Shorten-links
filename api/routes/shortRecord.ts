b6aec75c2df5246ba02409099173faff708afa91
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-08 18:20:53 +0600

    Написал логику получение ссылки по названию

--- api/routes/shortRecord.ts
+++ api/routes/shortRecord.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import shortRecord from "../models/ShortRecord";
 
 const shortRecordRouters = express.Router();
 
-shortRecordRouters.get('/:shortUrl', (req, res) => {
-  res.send('Сссылка')
+shortRecordRouters.get('/:shortUrl', async (req, res) => {
+  try {
+    const link = await shortRecord.findOne({shortUrl: req.params.shortUrl});
+
+    if (link) {
+      const url = link.originalUrl;
+
+      return res.status(301).redirect(url)
+    }
+  } catch {
+    return res.sendStatus(500)
+  }
 });
 
 shortRecordRouters.get('/links', (req, res) => {
