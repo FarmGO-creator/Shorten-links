01f4d6b76d2d850b92addd2df50b9ab8f0e6871b
Polushin Dima <dima2004polushin2004@gmail.com>
2023-02-08 23:30:37 +0600

    Написал логику отправки в контейнере и сгенерировал ссылку

--- frontend/src/containers/ShortLink/ShortLink.tsx
+++ frontend/src/containers/ShortLink/ShortLink.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Container, TextField, Button, Typography, Paper} from "@mui/material";
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
+import {selectUrl} from "../../store/shortLinkSlice";
+import {postLink} from "../../store/shortLinkThunk";
 
 const ShortLink = () => {
+  const [value, setValue] = useState('');
+  const dispatch = useAppDispatch();
+  const link = useAppSelector(selectUrl);
+  let newLink = null;
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await dispatch(postLink(value));
+  };
+
+  if (link) {
+    newLink = (
+      <a href={'http://localhost:8000/' + link.shortUrl}>http://localhost:3000/{link.shortUrl}</a>
+    )
+  }
+
+
   return (
-    <Box component='form' sx={{width: 400}}>
+    <Box component='form' sx={{width: 400}} onSubmit={onSubmit}>
       <Container>
         <Paper elevation={3} sx={{p: 5}}>
           <Typography variant="h5" component="p" sx={{mb: 3}}>
@@ -16,11 +36,14 @@ const ShortLink = () => {
             type='url'
             required
             fullWidth
+            value={value}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+
           />
           <Button type='submit' sx={{mt: 2}} variant="contained">Contained</Button>
         </Paper>
         <Typography variant="h5" component="div">
-          Ссылка:
+          Ссылка: {newLink}
         </Typography>
       </Container>
     </Box>
